563c641e61b38969a05e87f037dc55422540f730
lilfishyswimmer <hdemendoza@ucsd.edu>
2025-03-01 15:18:13 -0800

    setup data files

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
+from glob import glob
 from setuptools import setup
+
 
 package_name = '148_gestures'
 
@@ -6,6 +9,12 @@ setup(
     name=package_name,
     version='0.0.1',
     packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     author='root',
